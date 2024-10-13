6d2be16251729ac8332edc883be30033f8413543
deepak1051 <deepakpanwar1051@gmail.com>
2024-10-13 16:12:12 +0530

    req buffer stream

--- index.js
+++ index.js
@@ -3,7 +3,36 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`
+    
+    <form  method="POST" action="/">
+      <input placeholder="Email" name="email"/>
+      <input placeholder="Password" name="password"/>
+      <input placeholder="Password Confirmation" name="passwordConfirm"/>
+
+      <button>Sign Up</button>
+    </form>
+
+  `);
+});
+
+app.post('/', (req, res) => {
+  req.on('data', (data) => {
+    const parsed = data.toString('utf8').split('&');
+
+    const formData = {};
+
+    for (let item of parsed) {
+      const [key, value] = item.split('=');
+      formData[key] = value;
+    }
+
+    console.log(formData);
+  });
+
+  //get access to the form data
+
+  res.send('Thanks for signing up!');
 });
 
 const PORT = process.env.PORT || 5000;
